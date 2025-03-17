9735f05fbf951ec25bced0cd0a5a23797c55cdeb
miad01\lixinwu <lixinwu@mitac.com.tw>
2025-03-17 10:54:21 +0800

    更新table schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 create table coin(
   coinNo int not null AUTO_INCREMENT,
-  coinType varchar(10) not null,
+  coinType varchar(10) not null UNIQUE,
   coinChineseName varchar(10) not null,
-  PRIMARY KEY (coinNo)
+  PRIMARY KEY (coinNo),
+  CONSTRAINT coinTypeUnique UNIQUE(coinType)
 );
